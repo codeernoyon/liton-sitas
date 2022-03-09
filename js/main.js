d43ffca87ffdceeb2e9155603380a05ee2ac73dd
Nayan Islam <nayannk103@gmail.com>
2022-03-09 21:16:01 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// for Mobile Menu//
+
+var navLinks = document.getElementById("mobile_menu");
+function showMenu(){
+    navLinks.style.right = "0px";
+}
+function hideMenu(){
+    navLinks.style.right = "-250px";
+}
