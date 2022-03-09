61fe374df1add38cf521a79771c1588e26494708
Nayan Islam <nayannk103@gmail.com>
2022-03-10 01:18:55 +0600

    side scroll problem solve

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 // for Mobile Menu//
 
-var navLinks = document.getElementById("mobile_menu");
+const navLinks = document.getElementById("mobile_menu");
+const header = document.getElementsByClassName("header");
 function showMenu(){
     navLinks.style.right = "0px";
 }
 function hideMenu(){
     navLinks.style.right = "-250px";
 }
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY === 100){
+        header.style.background = "red";
+    }
+})
