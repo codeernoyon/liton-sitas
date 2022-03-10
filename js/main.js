932eba4957eef39f8208412c59c5a7ac739ffbe7
Nayan Islam <nayannk103@gmail.com>
2022-03-10 22:57:30 +0600

    add header background

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
 // for Mobile Menu//
 
 const navLinks = document.getElementById("mobile_menu");
-const header = document.getElementsByClassName("header");
-function showMenu(){
-    navLinks.style.right = "0px";
-}
-function hideMenu(){
-    navLinks.style.right = "-250px";
-}
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+// function showMenu(){
+//     navLinks.style.right = "0px";
+// }
+// function hideMenu(){
+//     navLinks.style.right = "-250px";
+// }
 
-window.addEventListener("scroll", () => {
-    if(window.scrollY === 100){
-        header.style.background = "red";
-    }
-})
+
+
+const headerOption = {
+    threshold: .97,
+    rootMargin: "0px",
+  };
+  
+  const headerObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("style");
+      } else {
+        header.classList.remove("style");
+      }
+    });
+  }, headerOption);
+  
+  headerObserver.observe(hero);
+  
+  const heroOption = {
+    threshold: 0,
+    rootMargin: "0px",
+  };
+  
+  const heroObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        hero.classList.add("style");
+      }
+    });
+  }, heroOption);
+  
+  heroObserver.observe(hero);
